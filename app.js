54bc7a9b08f21209d556024ebc3230c6e3202ea9
Elad Rovek <elad@vsecnow.com>
2021-12-05 13:31:40 +0200

    before action

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-var http = require('http');
-var url = require('url');
+var http = require ('http');
+var url = require ('url');
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
