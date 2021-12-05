556dc76bbfed00d05b109051524245663d598e4b
Elad Rovek <elad@vsecnow.com>
2021-12-05 12:58:20 +0200

    work

---
+++ app.js
@@ -0,0 +1,12 @@
+var http = require('http');
+var url = require('url');
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  var q = url.parse(req.url, true).query;
+  if (q.name === undefined) {
+	q.name = "World";
+	}
+  var txt = "hello" + " " + q.name ;
+  res.end(txt);
+}).listen(3000);
